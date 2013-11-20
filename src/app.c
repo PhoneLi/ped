dc768516841fbe347e7ecda96e06dc189c0b8cbf
PhoneLi <priscilla-zz@qq.com>
2013-11-20 19:26:57 +0800

    finish pmalloc

--- src/app.c
+++ src/app.c
@@ -1,8 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pmalloc.h"
+
+void
+putestInitWithFuncs(void (**_fun)(void) , int index){
+    _fun[index - 48]();
+    return ;
+}
+
+void
+pmalloc_test(){
+    printf("char : %zu \nsize : %zu \n" , sizeof(char) , sizeof(size_t));
+
+    char * dm = (char *)pmalloc(sizeof(char) * 512);
+    strcpy(dm , "hello world. - pmalloc");
+    printf("dm : %s \n" , dm);
+    pfree(dm);
+    
+    dm  = (char *)pcalloc(sizeof(char) * 512);
+    strcpy(dm , "hello world. - pcalloc.");
+    printf("dm : %s \n" , dm);
+    pfree(dm);
+    
+    dm  = (char *)pmalloc(sizeof(char) * 10);
+    printf("dm size : %zu \n" , pmalloc_size(dm));
+    dm =  prealloc(dm , sizeof(char) * 20);
+    printf("dm size : %zu \n" , pmalloc_size(dm));
+    pfree(dm);
+
+    char * pl = pstrdup("hello w");
+    printf("pl : %s \n" , pl);
+    printf(" userd memory : %zu\n framentation_ratio : %lf\n rss : %zu\n private_dirty : %zu\n" , 
+           pmalloc_used_memory() , 
+           pmalloc_get_fragmentation_ratio() ,
+           pmalloc_get_rss() ,
+           pmalloc_get_private_dirty()
+           );
+    pfree(pl);
+    
+}
 
 int
-main(void)
+main(int argv , char * args[])
 {
+    if(argv > 1){
+        void (*fun[])(void) = {
+            pmalloc_test
+        };
+        putestInitWithFuncs(fun ,(int) *args[1]);
+    }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/pmalloc.h
+++ src/pmalloc.h
@@ -25,6 +25,7 @@ void   *pcalloc(size_t size);
 void   *prealloc(void *ptr, size_t size);
 void    pfree(void *ptr);
 char   *pstrdup(const char *s);
+size_t  pmalloc_size(void *ptr);
 size_t  pmalloc_used_memory(void);
 void    pmalloc_enable_thread_safeness(void);
 void    pmalloc_set_oom_handler(void (*oom_handler)(size_t));
